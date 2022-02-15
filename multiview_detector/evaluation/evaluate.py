15b7d9bd9cff683481cd5427f94ff88a8e67f2de
Jinwoo Hwang <jinwoo.hwang@deepingsource.io>
2022-02-15 17:51:49 +0900

    Remove Matlab evalutation block due to sporadic failure

--- multiview_detector/evaluation/evaluate.py
+++ multiview_detector/evaluation/evaluate.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 # import matlab.engine
-# from multiview_detector.evaluation.pyeval.evaluateDetection import evaluateDetection_py
+from multiview_detector.evaluation.pyeval.evaluateDetection import evaluateDetection_py
 
 
 # def matlab_eval(res_fpath, gt_fpath, dataset='wildtrack'):
@@ -17,19 +17,21 @@ import numpy as np
 #     modp, moda, recall, precision = evaluateDetection_py(res_fpath, gt_fpath, dataset)
 #     return recall, precision, moda, modp
 
-
+# Removed Matlab evaluation block due to sporadic errors in line 23
+# TODO: Identify why failure to import matlab.engine sometimes triggers a ModuleNotFoundError
 def evaluate(res_fpath, gt_fpath, dataset='wildtrack'):
-    try:
-        import matlab.engine
+    # try:
+    #     import matlab.engine
 
-        eng = matlab.engine.start_matlab()
-        eng.cd('multiview_detector/evaluation/motchallenge-devkit')
-        res = eng.evaluateDetection(res_fpath, gt_fpath, dataset)
-        recall, precision, moda, modp = np.array(res['detMets']).squeeze()[[0, 1, -2, -1]]
-    except:
-        from multiview_detector.evaluation.pyeval.evaluateDetection import evaluateDetection_py
+    #     eng = matlab.engine.start_matlab()
+    #     eng.cd('multiview_detector/evaluation/motchallenge-devkit')
+    #     res = eng.evaluateDetection(res_fpath, gt_fpath, dataset)
+    #     recall, precision, moda, modp = np.array(res['detMets']).squeeze()[[0, 1, -2, -1]]
+    # except:
+    #     from multiview_detector.evaluation.pyeval.evaluateDetection import evaluateDetection_py
 
-        recall, precision, moda, modp = evaluateDetection_py(res_fpath, gt_fpath, dataset)
+    #     recall, precision, moda, modp = evaluateDetection_py(res_fpath, gt_fpath, dataset)
+    recall, precision, moda, modp = evaluateDetection_py(res_fpath, gt_fpath, dataset)
     return recall, precision, moda, modp
 
 
