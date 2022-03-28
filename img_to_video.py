cdfbb3cc2160060db4f2c61521599d907d2de54e
luorix1 <jinwoo.hwang@deepingsource.io>
2022-03-28 14:52:22 +0900

    Add code that converts images to video

---
+++ img_to_video.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+import re
+
+
+path = '/workspace/MVDeTr_research/output'
+# path = '/workspace/MVDeTr_research/imgs'
+paths = [os.path.join(path, f'{i:08}.png') for i in range(1, 41)]
+
+fps = 5
+frame_array = []
+
+for file in paths:
+    img = cv2.imread(file)
+    height, width, layers = img.shape
+    size = (width, height)
+    print(file)
+    frame_array.append(img)
+
+out = cv2.VideoWriter(os.path.join(path, 'mvdet_sort.mp4'), cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+# out = cv2.VideoWriter(os.path.join(path, 'camera_view.mp4'), cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+
+for i in range(len(frame_array)):
+    out.write(frame_array[i])
+
+out.release()
